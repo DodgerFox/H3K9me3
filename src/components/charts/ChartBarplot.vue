d4ca38ea069779220c94a81bea271629b4f4dc3c
DESKTOP-V7ICETQ\LilBorsch <darkdodgerfox@yandex.ru>
2020-10-23 12:49:11 +0300

    fix chartBarplot

--- src/components/charts/ChartBarplot.vue
+++ src/components/charts/ChartBarplot.vue
@@ -36,6 +36,8 @@ export default {
                         type: 'linear',
                         position: 'top',
                         ticks: {
+                            beginAtZero: true,
+                            stepSize: 10,
                             display: false
                         }
                     },
@@ -62,49 +64,29 @@ export default {
         init() {
             this.chartdata = {
                 labels: [],
-                datasets: [
-                    {
-                    type: 'scatter',
-                    label: 'Scatter',
-                    data: [{
-            "x": 1.54731865,
-            "y": "0.6252581"
-          },
-          {
-            "x": 1.53064353,
-            "y": "0.60509"
-          },
-          {
-            "x": 1.52027183,
-            "y": "0.61584634"
-          }],
-                    xAxisID:'left-x-axis',
-                    borderColor: colors,
-                    backgroundColor: colors
-                }
-                ],
+                datasets: [],
             };
             let data = this.data;
             console.log(data, 'daatata');
             this.chartdata.labels = this.labels
-            let colors = ['#ACDEE7', '#CCAACB', '#FFFFB5', '#FFCDB6', '#ECECEC', '#C7DBDA', '#FEFEFE', '#FDD7C2', '#818181']
-            data.forEach((element) => {
+            let colors = ['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9', '#3498DB', '#1ABC9C', '#16A085', '#16A085', '#27AE60', '#2ECC71', '#F1C40F', '#F39C12', '#D35400', '#34495E', '#2C3E50', '#A93226', '#CB4335', '#884EA0', '#7D3C98', '#2471A3', '#2E86C1', '#17A589', '#28B463']
+            data.forEach((element, index) => {
                 console.log(element);
-                // this.chartdata.datasets.push({
-                //     type: 'scatter',
-                //     label: 'Scatter',
-                //     data: element,
-                //     xAxisID:'left-x-axis',
-                //     borderColor: colors,
-                //     backgroundColor: colors
-                // })
-                // this.chartdata.datasets.push({
-                //     type: 'bar',
-                //     label: 'bar',
-                //     data: [],
-                //     xAxisID:'bar-x-axis',
-                //     backgroundColor: 'red'
-                // })
+                this.chartdata.datasets.push({
+                    type: 'scatter',
+                    label: 'Scatter',
+                    data: element,
+                    xAxisID:'left-x-axis',
+                    borderColor: colors[index],
+                    backgroundColor: colors[index]
+                })
+                this.chartdata.datasets.push({
+                    type: 'bar',
+                    label: 'bar',
+                    data: [],
+                    xAxisID:'bar-x-axis',
+                    backgroundColor: 'red'
+                })
             });
             this.renderChart(this.chartdata, this.options)
         }
--- src/views/LncRNA.vue
+++ src/views/LncRNA.vue
@@ -5,20 +5,20 @@
             <div class="wrap">
                 <h2 class="section-title">LnkRNA: {{$route.params.id}}</h2>
                 <div class="block">
-                  <!-- <div class="block-wrap" v-if="lncrna">
+                  <div class="block-wrap" v-if="lncrna">
                       <h4>{{lncrna.chart.title}}</h4>
                       <ChartPeaks :data="lncrna.chart" />
-                  </div> -->
-                  <!-- <div class="block-none" v-else>
+                  </div>
+                  <div class="block-none" v-else>
                       <h4>There is no data</h4>
-                  </div> -->
+                  </div>
                 </div>
                 <v-select v-if="barplot" :options="histones" v-model="active"></v-select>
                 <div class="block" v-if="barplot">
                   <ChartBarplot :data="barplot" :labels="lncrna.barplot.labels" />
                 </div>
-                <!-- <Links v-if="lncrna" :data="lncrna.links" />
-                <Table :max="10" v-if="lncrna" :data="getData.table || lncrna.table" /> -->
+                <Links v-if="lncrna" :data="lncrna.links" />
+                <Table :max="10" v-if="lncrna" :data="getData.table || lncrna.table" />
             </div>
         </section>
         <Loader />
@@ -28,24 +28,23 @@
 <script>
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-// import Table from '@/components/Table';
-// import Links from '@/components/Links';
+import Table from '@/components/Table';
+import Links from '@/components/Links';
 import Loader from '@/components/Loader';
-// import ChartPeaks from '@/components/charts/ChartPeaks'
+import ChartPeaks from '@/components/charts/ChartPeaks'
 import ChartBarplot from '@/components/charts/ChartBarplot'
 import vSelect from 'vue-select'
-import lncrna from '@/data/lncreal.json'
 
 export default {
   name: 'lncrna',
   components: {
     Header,
-    // Table,
-    // Links,
+    Table,
+    Links,
     Loader,
     Footer,
     ChartBarplot,
-    // ChartPeaks,
+    ChartPeaks,
     vSelect
   },
   data() {
@@ -65,8 +64,7 @@ export default {
   },
   async mounted () {
     this.$store.dispatch('setLoader', true)
-    this.lncrna = lncrna;
-    // this.lncrna = await this.$store.dispatch('fetchLncrna', [this.$route.params.id, 1, 10])
+    this.lncrna = await this.$store.dispatch('fetchLncrna', [this.$route.params.id, 1, 10])
     this.$store.dispatch('setLoader', false)
     let count = false;
     this.histones = [];
